b8ec05d59005497b9fbc499b7e0ccbc0e9b55df3
kishlay <kishlay141@gmail.com>
2025-06-16 18:19:46 +0530

    updated the  diffParser.ts

--- src/diffParser.ts
+++ src/diffParser.ts
@@ -2,3 +2,24 @@ export interface DiffBlock {
   added: string[];
   removed: string[];
 }
+
+export function parseDiff(rawDiff: string): DiffBlock[] {
+  const lines = rawDiff.split("\n");
+  const blocks: DiffBlock[] = [];
+  let currentBlock: DiffBlock = { added: [], removed: [] };
+
+  lines.forEach((line) => {
+    if (line.startsWith("+")) {
+      currentBlock.added.push(line.substring(1));
+    } else if (line.startsWith("-")) {
+      currentBlock.removed.push(line.substring(1));
+    } else {
+      if (currentBlock.added.length > 0 || currentBlock.removed.length > 0) {
+        blocks.push(currentBlock);
+        currentBlock = { added: [], removed: [] };
+      }
+    }
+  });
+
+  return blocks;
+}
